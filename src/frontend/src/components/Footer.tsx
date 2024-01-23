3d1b8e7d7f83971f4b1baee987b9da542d2efb8a
rusmanov <usmanov.riyaz22@gmail.com>
2024-01-23 10:49:42 +0100

    fix: fixed footer

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -3,23 +3,21 @@ import { setColors } from "../styles/colors";
 
 const Footer = () => {
   return (
-    <Box
-    sx={styles.box}
-    >
-      <Typography>Copyright @2022</Typography>
+    <Box sx={styles.box}>
+      <Typography>@matcha 2024 abarot & riazus</Typography>
     </Box>
   );
 };
 
 const styles = {
   box: {
-    backgroundColor: setColors("lightGray"), 
+    backgroundColor: setColors("lightGray"),
     height: "4rem",
     boxShadow: 15,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-  }
-}
+  },
+};
 
 export default Footer;
